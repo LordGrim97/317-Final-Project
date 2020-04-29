74ae200f878c81f10fb22e361016b098b5bc8741
Cameron Stark <starkc920@outlook.com>
2020-04-28 20:56:25 -0400

    added stuff

--- api/api.js
+++ api/api.js
@@ -3,6 +3,7 @@ const {
     Request
 } = require("tedious");
 
+// const mysql = require('mysql');
 
 const http = require('http')
 
@@ -12,55 +13,245 @@ const express = require('express');
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const server = http.createServer(app)
 
-// const config = {
-//     authentication: {
-//         options: {
-//             userName: "username",
-//             password: "password"
-//         },
-//         type: "default"
-//     },
-//     server: "your_server.database.windows.net",
-//     options: {
-//         database: "your_database",
-//         encrypt: true
+// var config = {
+//     host: '317.database.windows.net',
+//     user: 'dcribbs',
+//     password: 'Audi1997',
+//     database: '317FinalProject',
+//     port: 1433,
+//     ssl: true,
+// }
+
+// pool.getConnection(
+//     function(err, conn) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Connection Sucess");
+//         }
 //     }
-// };
+// )
+
+const config = {
+    authentication: {
+        options: {
+            userName: "dcribbs",
+            password: "Audi1997"
+        },
+        type: 'default'
+    },
+    server: "317.database.windows.net",
+    options: {
+        database: '317FinalProject',
+        encrypt: true,
+        rowCollectionOnRequestCompletion: true,
+        rowCollectionOnDone: true
+    }
+};
+
+const connection = new Connection(config);
+connection.on("connect", err => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log("Connection Successs");
+        //queryTest()
+    }
+})
+
+
+function queryTest() {
+    items = [];
+
+    const request = new Request(
+        "SELECT * FROM Recipes",
+        (err, rowCount) => {
+            if (err) {
+                console.log(err)
+            } else {
+                // console.log(`${rowCount} row(s) returned`);
+            }
+        }
+    );
+    
+    request.on('row', columns => {
+        columns.forEach(column => {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+    
+}   
+
 
 server.listen(port, () => {
     console.log("Server Started");
 
-    // const connection = new Connection(config);
 
-    // connection.on("connect", err => {
-    //     if (err) {
-    //         console.error(err.message)
-    //     } else {
-    //         console.log("Database Connection Succesful")
-    //     }
-    // })
+
+    
 })
 
 
-app.get("/test", (req, res, next) => {
-    res.json(["Test1", "Test2", "Test3"])
+function executeInsert(query) {
+    const request = new Request(
+        query, 
+        (err, rowCount) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("complete");
+            }
+        }
+    )
+
+    connection.execSql(request);
+}
+
+function execute(query, lastCol, callback) {
+    var items = [];
+    const request = new Request(
+        query,
+        (err, rowCount) => {
+            if (err) {
+                console.log(err)
+            } else {
+                callback(items);
+                console.log(`${rowCount} row(s) returned`);
+            }
+        }
+    );
+    
+    row = {}
+    request.on('row', columns => {
+        columns.forEach(column => {
+            row[column.metadata.colName] = column.value;
+            if (row[lastCol] != null) {
+                items.push(row);
+                row = {}
+            }
+        });
+    });
+    
+    connection.execSql(request);
+}
+
+// app.get("/test", (req, res, next) => {
+//     query = "SELECT * FROM Recipes";
+
+//     executeRecipeTable(query, function(items) {
+//         res.json(items);
+//     });
+// })
+
+app.get("/getRecipeNames/:item", (req, res, next) => {
+    var item = req.params.item;
+
+    var query = `
+        SELECT
+            RecipeId,
+            RecipeImage,
+            RecipeName
+        FROM Recipes
+        WHERE Ingredients LIKE '%` + item + "%'"; 
+
+    lastCol = "RecipeName";
+    //console.log(query);
+    execute(query, lastCol, function(items) {
+        res.json(items);
+    });
+
 })
 
-// function test() {
-//     const request = new Request(
-//         "query",
-//         (err, rowCount) => {
-//             if (err) {
-//                 console.error(err.message);
-//             } else {
-//                 console.log("${rowCount} rows returned");
-//             }
-//         }
-//     )
+app.get("/getRecipeDetails/:id", (req, res, next) => {
+    var id = req.params.id;
 
-//     Connection.execSql(request);
-// }
+    var query = `
+        SELECT 
+            *
+        FROM Recipes
+        WHERE RecipeId = '` + id + "'";
 
+    lastCol = "RecipeName";
+    execute(query, lastCol, function(items) {
+        res.json(items);
+    });
+})
+
+app.get('/login/:username/:password', (req, res, next) => {
+    
+    var username = req.params.username;
+    var password = req.params.password;
+
+    var query = `
+    SELECT 
+        UserId,
+        Username
+    FROM Users
+    WHERE Username = '` + username + "' AND  Pass = '" + password +  "'";
+
+    lastCol = "Username";
+    execute(query, lastCol, function(item) {
+        res.json(item);
+    })
+})
+
+app.get("/getUserReviews/:id", (req, res, next) => {
+    var id = req.params.id;
+
+    var query = `
+    SELECT 
+        A.RecipeName AS RecipeName,
+        B.ReviewScore AS ReviewScore
+    FROM Recipes A JOIN Reviews B
+        ON A.RecipeId = B.RecipeId
+    WHERE B.UserId = '` + id + "'";
+
+    lastCol = "ReviewScore";
+    execute(query, lastCol, function(items) {
+        res.json(items);
+    });
+})
+
+app.get("/getReviews/:id", (req, res, next) => {
+    var id = req.params.id;
+
+    var query = `
+    SELECT
+        B.ReviewScore AS ReviewScore,   
+        A.Username AS Username
+    FROM Users A JOIN Reviews B
+        ON A.UserId = B.UserId
+    WHERE B.RecipeId = ` + id + "";
+
+    console.log(query);
+    lastCol = "Username";
+    execute(query, lastCol, function(items) {
+        res.json(items);
+    });
+})
+
+app.get("/addReview/:score/:user/:recipe", (req, res, next) => {
+    var score = req.params.score;
+    var user = req.params.user;
+    var recipe = req.params.recipe;
+
+    var query = `
+    INSERT INTO Reviews (
+        RecipeId,
+        ReviewScore,
+        UserId
+    ) VALUES (` + recipe + ", " + score + ", " + user + ")";
+
+
+    executeInsert(query);
+})
 
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,131 @@
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, $http) {
 
-    $scope.test = 'Hello World';
+    $scope.title = "Find That Recipe";
+    $scope.isLoggedIn = false;
+    $scope.isGuest = false;
+    $scope.sidebarActive = false;
+    
+
+    $scope.isRecipeDetails = false;
+    $scope.isProfileDetails = false;
+
+    $scope.optionSelected = 0;
+
+
+    $scope.search = {
+        text: ""
+    };
+
+    $scope.loginInfo = {
+        username: "",
+        password: ""
+    }
+    $scope.user = {
+        userId: "",
+        username: ""
+    }
+    $scope.login = function() {
+        console.log($scope.loginInfo);
+        $http({
+            method: "GET",
+            url: baseUrl + "/login/" + $scope.loginInfo.username + "/" + $scope.loginInfo.password
+        }).then(function(response) {
+            //console.log(response);
+            if (response.length != 0) {
+                $scope.isGuest = false;
+                $scope.isLoggedIn = true;
+                
+                $scope.user.userId = response.data[0].UserId;
+                $scope.user.username = response.data[0].Username;
+            } else {
+
+            }
+        }, function(error) {
+
+        })
+    }
+
+
+    $scope.userReviews = [];
+    $scope.profileClicked = function() {
+        $scope.sidebarActive = !$scope.sidebarActive;
+
+        $http({
+            method: "GET",
+            url: baseUrl + "/getUserReviews/" + $scope.user.userId
+        }).then(function(response) {
+            $scope.userReviews = response.data;
+            $scope.isProfileDetails = true;
+            $scope.isRecipeDetails = false;
+        }, function(error) {
+
+        })
+    }
+
+    var baseUrl = "http://localhost:3100";
+    $scope.recipes = [];
+    $scope.searchRecipes = function() {
+
+        $http({
+            method: "GET",
+            url: baseUrl + "/getRecipeNames/" + $scope.search.text
+        }).then(function(response) {
+            //console.log(response.data);
+            $scope.recipes = response.data;
+        }, function(error) {  
+            console.error(error);
+        })
+
+    }
+
+    $scope.recipeDetails = [];
+    $scope.getRecipeDetails = function(recipeId) {
+        //console.log(recipeId);
+        $scope.sidebarActive = !$scope.sidebarActive;
+
+        $http({
+            method: "GET",
+            url: baseUrl + "/getRecipeDetails/" + recipeId
+        }).then(function(response) {
+            $scope.recipeDetails = response.data;
+            $scope.isProfileDetails = false;
+            $scope.isRecipeDetails = true;
+        }, function(error) {
+            console.error(error);
+        })
+    }
+    $scope.splitIngredients = function(ingredients) {
+        return ingredients.split(",");
+    }
+    $scope.splitDirections = function(directions) {
+        return directions.split(".");
+    }
+
+    $scope.recipeReviews = [];
+    $scope.getRecipeReviews = function(recipeId) {
+        $http({
+            method: "GET",
+            url: baseUrl + "/getReviews/" + recipeId
+        }).then(function(response) {
+            console.log(response.data);
+            $scope.recipeReviews = response.data;
+        }, function(error) {
+
+        })
+    }
+
+
+    $scope.review = {
+        score: null,
+        recipeId: null,
+        userId: null
+    };
+    $scope.addReview = function(recipeId) {
+        $http({
+            method: "GET",
+            url: baseUrl + "/addReview/" + $scope.review.score + "/" + $scope.user.userId + '/' + recipeId
+        }).then(function(response) {
+            $scope.review.score = null
+        })
+    }
 });
